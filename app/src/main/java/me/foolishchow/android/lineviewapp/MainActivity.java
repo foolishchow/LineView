a3a99c28993982a8130e0631a40169bd0a66701f
foolishchow <foolishchow@dingtalk.com>
2020-12-14 13:26:59 +0800

    SubScriptedTextView

--- app/src/main/java/me/foolishchow/android/lineviewapp/MainActivity.java
+++ app/src/main/java/me/foolishchow/android/lineviewapp/MainActivity.java
@@ -1,34 +1,32 @@
 package me.foolishchow.android.lineviewapp;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mSS;
+    //private TextView mSS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mSS = findViewById(R.id.ss);
+        //mSS = findViewById(R.id.ss);
         //mSS.postDelayed(mUpdater, 200L);
     }
 
-    private int index = 1;
-    private Runnable mUpdater = new Runnable() {
-        @SuppressLint("SetTextI18n")
-        @Override
-        public void run() {
-            if (mSS != null) {
-                mSS.setText("[ " + index + " -- " + Double.valueOf(Math.random()).floatValue() * 1000 + " ]");
-                index ++;
-                mSS.postDelayed(mUpdater, 200L);
-            }
-        }
-    };
+    //private int index = 1;
+    //private Runnable mUpdater = new Runnable() {
+    //    @SuppressLint("SetTextI18n")
+    //    @Override
+    //    public void run() {
+    //        if (mSS != null) {
+    //            mSS.setText("[ " + index + " -- " + Double.valueOf(Math.random()).floatValue() * 1000 + " ]");
+    //            index ++;
+    //            mSS.postDelayed(mUpdater, 200L);
+    //        }
+    //    }
+    //};
 
 }
